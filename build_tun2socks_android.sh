63437a329a54ab998c0f8f7740f0c1271acf8e9a
Mohammad Reza Mokhtarabadi <mmokhtarabadi@gmail.com>
2021-09-23 21:51:41 +0330

    add universal-toolchain as module

---
+++ build_tun2socks_android.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export ANDROID_USE_SHARED_LIBC=OFF
+. universal-android-toolchain/toolchain.sh "$@"
+
+rm -rf build && mkdir build && cd build
+
+export CFLAGS="$CFLAGS -DBADVPN_LINUX -DBADVPN_USE_SIGNALFD -DBADVPN_USE_EPOLL -DNDEBUG"
+
+android_cmake_command \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_NOTHING_BY_DEFAULT=ON \
+    -DBUILD_TUN2SOCKS=ON \
+    ..
+
+"$CMAKE/bin/cmake" --build . --config Debug
+
+cp tun2socks/libbadvpn-tun2socks.a $OUTPUT_DIR/
+
+cp $CURRENT_DIR/tun2socks/tun2socks.h $OUTPUT_DIR/../
