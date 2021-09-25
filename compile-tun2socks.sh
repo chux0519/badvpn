2cf0b958359406f7174c059de24b5ad40bc0439f
Mohammad Reza Mokhtarabadi <mmokhtarabadi@gmail.com>
2021-09-25 11:36:22 +0330

    build static library instead shared

--- compile-tun2socks.sh
+++ compile-tun2socks.sh
@@ -1,57 +1,16 @@
-#!/usr/bin/env bash
-#
-# Compiles tun2socks for Linux.
-# Intended as a convenience if you don't want to deal with CMake.
+#!/bin/bash
 
-# Input environment vars:
-#   SRCDIR - BadVPN source code
-#   OUTDIR - tun2socks binary output file directory
-#   CC - compiler
-#   CFLAGS - compiler compile flags
-#   LDFLAGS - compiler link flags
-#   ENDIAN - "little" or "big"
-#   KERNEL - "2.6" or "2.4", default "2.6"
-#
-# Puts object files and the executable in the working directory.
-#
+export ANDROID_USE_SHARED_LIBC=OFF
+. universal-android-toolchain/toolchain.sh "$@"
 
-if [[ -z $SRCDIR ]] || [[ ! -e $SRCDIR/CMakeLists.txt ]]; then
-    echo "SRCDIR is wrong"
-    exit 1
-fi
+rm -rf build && mkdir build && cd build
 
-if [[ ! -z $OUTDIR ]] && [[ ! -d $OUTDIR  ]]; then
-    echo "OUTDIR is wrong"
-    exit 1
-fi
+SRCDIR="../"
 
-if ! "${CC}" --version &>/dev/null; then
-    echo "CC is wrong"
-    exit 1
-fi
+CFLAGS="${CFLAGS} -fPIC -std=gnu99"
+INCLUDES=("-I${SRCDIR}" "-I${SRCDIR}/lwip/src/include/ipv4" "-I${SRCDIR}/lwip/src/include/ipv6" "-I${SRCDIR}/lwip/src/include" "-I${SRCDIR}/lwip/custom")
+DEFS=(-DBADVPN_THREAD_SAFE=0 -DBADVPN_LINUX -DBADVPN_BREACTOR_BADVPN -D_GNU_SOURCE -DBADVPN_USE_SIGNALFD -DBADVPN_USE_EPOLL -DBADVPN_LITTLE_ENDIAN)
 
-if [[ $ENDIAN != "little" ]] && [[ $ENDIAN != "big" ]]; then
-    echo "ENDIAN is wrong"
-    exit 1
-fi
-
-if [[ -z $KERNEL ]]; then
-    KERNEL="2.6"
-elif [[ $KERNEL != "2.6" ]] && [[ $KERNEL != "2.4" ]]; then
-    echo "KERNEL is wrong"
-    exit 1
-fi
-
-CFLAGS="${CFLAGS} -std=gnu99"
-INCLUDES=( "-I${SRCDIR}" "-I${SRCDIR}/lwip/src/include/ipv4" "-I${SRCDIR}/lwip/src/include/ipv6" "-I${SRCDIR}/lwip/src/include" "-I${SRCDIR}/lwip/custom" )
-DEFS=( -DBADVPN_THREAD_SAFE=0 -DBADVPN_LINUX -DBADVPN_BREACTOR_BADVPN -D_GNU_SOURCE )
-
-[[ $KERNEL = "2.4" ]] && DEFS=( "${DEFS[@]}" -DBADVPN_USE_SELFPIPE -DBADVPN_USE_POLL ) || DEFS=( "${DEFS[@]}" -DBADVPN_USE_SIGNALFD -DBADVPN_USE_EPOLL )
-
-[[ $ENDIAN = "little" ]] && DEFS=( "${DEFS[@]}" -DBADVPN_LITTLE_ENDIAN ) || DEFS=( "${DEFS[@]}" -DBADVPN_BIG_ENDIAN )
-
-[[ -z $OUTDIR ]] && OUTDIR="."
-    
 SOURCES="
 base/BLog_syslog.c
 system/BReactor_badvpn.c
@@ -120,7 +79,7 @@ OBJS=()
 for f in $SOURCES; do
     obj=${f//\//_}.o
     "${CC}" -c ${CFLAGS} "${INCLUDES[@]}" "${DEFS[@]}" "${SRCDIR}/${f}" -o "${obj}"
-    OBJS=( "${OBJS[@]}" "${obj}" )
+    OBJS=("${OBJS[@]}" "${obj}")
 done
 
-"${CC}" ${LDFLAGS} "${OBJS[@]}" -o $OUTDIR/tun2socks -lrt -lpthread
+$AR rcs $OUTPUT_DIR/libbadvpn.a "${OBJS[@]}"
